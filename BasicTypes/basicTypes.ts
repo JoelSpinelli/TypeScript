f1b8ac45cb86c31ac7d0e6bb9673cdec49d1025c
JoelSpinelli <joel.spinelli@hotmail.com>
2020-01-25 11:53:45 -0300

    boolean and numbers

---
+++ BasicTypes/basicTypes.ts
@@ -0,0 +1,8 @@
+// Boolean
+let isDone: boolean = false;    // js: var isDone = false;
+
+// Number
+let decimal: number = 6;        // js: var decimal = 6;
+let hex: number = 0xf00d;       // js: var hex = 0xf00d;
+let binary: number = 0b1010;    // js: var binary = 10;
+let octal: number = 0o744;      // js: var octal = 484;
